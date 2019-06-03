bcc084a4f5496e42456857bf74dc35fdf9e4262c
Thomas Johnson <tjohnson1106@gmail.com>
2019-06-03 15:31:31 -0400

    created side bar function al component

---
+++ src/ui/SideBar/SideBarHeader/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Menu } from "semantic-ui-react";
+
+import "./SideBarHeader.scss";
+import { StringLiteral } from "@babel/types";
+
+interface Props {
+  heading: string | boolean;
+  title: string;
+}
+
+export function SideBarHeader(props: Props) {
+  const heading = props.title ? props.title.toUpperCase() : "";
+
+  return (
+    <Menu.Item>
+      <Menu.Header className="side-bar-header">{heading}</Menu.Header>
+    </Menu.Item>
+  );
+}
--- src/ui/SideBar/index.tsx
+++ src/ui/SideBar/index.tsx
@@ -1,25 +1,26 @@
 import React, { Component } from "react";
-import { Menu } from "semantic-ui-react";
+import { Menu, Divider } from "semantic-ui-react";
 
-// import SideBarItem from "./SideBarItem"
-import "./SideBar.scss";
 import { SideBarItem } from "./SideBarItem";
+import "./SideBar.scss";
 
 class SideBar extends Component {
-  state = {};
   render() {
     return (
       <Menu borderless vertical stackable fixed="left" className="side-nav">
         <SideBarItem highlight={true} label="Home" icon="home" />
         <SideBarItem label="Trending" icon="fire" />
         <SideBarItem label="Followers" icon="spy" />
+        <Divider />
         <SideBarItem label="History" icon="history" />
         <SideBarItem label="Watch later" icon="clock" />
         <SideBarItem label="Liked videos" icon="thumbs up" />
+        <Divider />
         <SideBarItem label="Movies and Shows" icon="film" />
         <SideBarItem label="Report history" icon="flag" />
         <SideBarItem label="Help" icon="help circle" />
         <SideBarItem label="Send feedback" icon="comment" />
+        <Divider />
       </Menu>
     );
   }
