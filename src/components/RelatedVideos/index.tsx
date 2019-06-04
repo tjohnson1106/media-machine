13c27ed1671471f1b9fe6b885e99f3fa26f7821d
Thomas Johnson <tjohnson1106@gmail.com>
2019-06-04 15:49:57 -0400

    implemented next videos in related videos

--- src/components/RelatedVideos/index.tsx
+++ src/components/RelatedVideos/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import VideoPreview from "../VideoPreview";
+import { NextUpVideo } from "../RelatedVideos/NextUpVideo";
 import "./RelatedVideos.scss";
 
 export function RelatedVideos() {
   return (
     <div className="related-videos">
+      <NextUpVideo />
       <VideoPreview horizontal={true} />
       <VideoPreview horizontal={true} />
       <VideoPreview horizontal={true} />
