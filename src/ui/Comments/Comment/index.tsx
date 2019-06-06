353c5f87ced23840ac8a54a2192ea7a4e61b58bc
Thomas Johnson <tjohnson1106@gmail.com>
2019-06-06 16:02:49 -0400

    created comment function

---
+++ src/ui/Comments/Comment/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Button, Image } from "semantic-ui-react";
+
+import { Rating } from "../../../components/Rating";
+import "./Comment.scss";
+
+// add props
+export function Comment() {
+  return (
+    <div className="comment">
+      <Image
+        className="user-image"
+        src="http://via.placeholder.com/48x48"
+        circular
+      />
+      <div>
+        <div className="user-name">User name</div>
+        <span>Comment text</span>
+        <div className="comment-action">
+          <Rating likeCount={1} />{" "}
+          <Button size="mini" compact>
+            REPLY
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
