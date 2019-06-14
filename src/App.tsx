69b86b6c4e743d0a181384685f5ca4125b69f5aa
Thomas Johnson <tjohnson1106@gmail.com>
2019-06-14 16:45:00 -0400

    added lifecycle method in app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,50 @@
-import React from "react";
+import React, { Component, ReactNode } from "react";
 import { Route, Switch } from "react-router-dom";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
 
 import "./App.css";
 
 import Home from "./ui/Home";
 import { AppLayout } from "./components/AppLayout";
 import { Watch } from "./ui/Watch";
+import { youtubeLibraryLoaded } from "./store/actions/api";
 
-const App: React.FC = () => {
-  return (
-    <AppLayout>
-      <Switch>
-        <Route path="/watch" component={Watch} />
-        <Route path="/" component={Home} />
-      </Switch>
-    </AppLayout>
-  );
-};
+const API_KEY = "AIzaSyBeHDktLNXphSNGAkhOSXQ0FvGAjY1S0Hds";
+interface Youtube {
+  gapi: any;
+  youtubeLibraryLoaded: string;
+}
+
+class App extends Component<Youtube, {}> {
+  componentDidMount() {
+    this.loadYoutubeApi();
+  }
+
+  loadYoutubeApi() {
+    const script = document.createElement("script");
+    script.src = "https://apis.google.com/js/client.js";
+
+    script.onload = () => {
+      window.gapi.load("client", () => {
+        window.gapi.client.setApiKey(API_KEY);
+        window.gapi.client.load("youtube", "v3", () => {
+          this.props.youtubeLibraryLoaded();
+        });
+      });
+    };
+  }
+
+  render() {
+    return (
+      <AppLayout>
+        <Switch>
+          <Route path="/watch" component={Watch} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </AppLayout>
+    );
+  }
+}
 
 export default App;
